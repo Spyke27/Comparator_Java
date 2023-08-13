40eb45fbf5776dddc280ef693e7f5ffc802701c7
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-13 00:08:56 -0300

    Comparator

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
-import java.util.Scanner;
+
+import entities.Product;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		Scanner scan = new Scanner(System.in);
 		
-		String animais = "gato cachorro papagaio cavalo aranha baleia tigre";
-		String[] vetorAnimais = animais.split(" ");
+		List<Product> list = new ArrayList<>();
 		
-		for(int i = 0; i < vetorAnimais.length - 1; i++) {
-			System.out.println(vetorAnimais[i]);
+		list.add(new Product("TV", 1899.90));
+		list.add(new Product("Smartphone", 3259.90));
+		list.add(new Product("Xbox-One", 1250.00));
+		list.add(new Product("Notebook", 2999.90));
+		list.add(new Product("Acer Nitro", 7280.90));
+		
+		Comparator<Product> comparator = (p1, p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 		};
+		list.sort(comparator);
 		
-		scan.close();
+		for(Product p: list) {
+			System.out.println(p.toString());
+		}
 	}
 
 }
